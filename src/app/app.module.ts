21a4c2e2d035fee596b9ed8ffecc31346ac20795
Goswami <681468@cognizant.com>
2018-11-22 10:39:36 +0530

    angular assignment1 first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,71 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule } from '@angular/material';
+import { MatProgressBarModule, MatDialogModule, MatSnackBarModule, MatTooltipModule} from '@angular/material';
+import { MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
+import { MatAutocompleteModule, MatCheckboxModule, MatDatepickerModule, MatRadioModule, MatSelectModule } from '@angular/material';
+import { MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDividerModule, MatStepperModule, MatTabsModule } from '@angular/material';
+import { MatButtonToggleModule, MatChipsModule, MatProgressSpinnerModule } from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { NotesService } from './notes.service';
 
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    LayoutModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatListModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule
+  ],
+  providers: [NotesService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
